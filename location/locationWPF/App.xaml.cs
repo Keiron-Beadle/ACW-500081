b58e8ddf92d5cf1cdd6024ccafd145571908be14
Keiron-Beadle <K.BEADLE-2018@hull.ac.uk>
2021-02-25 12:15:01 +0000

    Added WPF support, going to merge back into 'location' now, saving before merge.

---
+++ location/locationWPF/App.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+namespace locationWPF
+{
+    public partial class App : Application
+    {
+        [DllImport("kernel32.dll")]
+        private static extern bool AttachConsole(int dwId);
+
+        [DllImport("kernel32.dll")]
+        private static extern bool FreeConsole();
+
+        private void Initialisation(object sender, StartupEventArgs e)
+        {
+            if (e.Args.Length == 0)
+            {
+                FreeConsole();
+                MainWindow wnd = new MainWindow();
+                wnd.Show();
+            }
+            else
+            {
+                try {
+                    AttachConsole(-1);
+                    ConsoleWindow.Main(e.Args);
+                    Environment.Exit(0);
+                }
+                catch { throw new Exception("No parent console to attach to."); }
+            }
+        }
+    }
+}
---
+++ location/locationWPF/WhoisManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Sockets;
+
+
+namespace locationWPF
+{
+    public static class WhoisManager
+    {
+        private static string person;
+        private static string location = null;
+        private static bool bPOST = false;
+        public static string CreateRequest(string person, string location)
+        {
+            WhoisManager.person = person;
+            WhoisManager.location = location;
+            if (location == null) { return person + "\r\n"; }
+            else { bPOST = true; return person + ' ' + location + "\r\n"; }
+        }
+
+        public static string HandleResponse(NetworkStream ns)
+        {      
+            int bytes;
+            string response = "";
+            System.Text.StringBuilder s = new System.Text.StringBuilder();
+            while ((bytes = ns.ReadByte()) != -1)
+            { s.Append((char)bytes); }
+            response = s.ToString();
+            if (bPOST && response.Equals("OK\r\n")) { return person + " location changed to be " + location + "\r\n"; }
+            else if (response.Equals("ERROR: no entries found\r\n")) { return response; }
+            else { return person + " is " + response; }
+        }
+    }
+}
